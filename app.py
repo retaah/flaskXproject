095c088234b4a85c352f950bf08634feb15a67d0
Maksim <Maskim.lion.by@gmail.com>
2022-06-14 19:04:52 +0300

    Add new files, new things

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'try-to-guess'
+
 from routes import *
 
 if __name__ == '__main__':
--- routes.py
+++ routes.py
@@ -1,11 +1,29 @@
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash
 
+messages = []
 
 @app.route('/')
 def main_page():
 
     return render_template('index.html')
 
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    if request.method == 'POST':
+        name = request.form['username']
+        email = request.form['email']
+        phone = request.form['phone_number']
 
+        if not name:
+            flash('Field "name" ')
+        elif not email:
+            flash('Field email ')
+        else:
+            messages.append({'username': name, 'email': email, 'phone': phone})
+            return redirect(url_for('main_page'))
+    return render_template('about.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', user_messages=messages)
