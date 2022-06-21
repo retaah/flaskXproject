0f6bec809866ff6b52e0aeed739ea56c1d807643
Maksim <Maskim.lion.by@gmail.com>
2022-06-21 18:55:43 +0300

    Initial commit

--- routes.py
+++ routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template, redirect, url_for, request, flash
+import sqlite3
 
-messages = []
+
+def db_connect():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 @app.route('/')
 def main_page():
@@ -16,14 +22,25 @@ def about():
         phone = request.form['phone_number']
 
         if not name:
-            flash('Field "name" ')
+            flash('Field "name" ', 'warning')
         elif not email:
-            flash('Field email ')
+            flash('Field email ', 'warning')
         else:
-            messages.append({'username': name, 'email': email, 'phone': phone})
-            return redirect(url_for('main_page'))
+            try:
+                connection = db_connect()
+                connection.execute('INSERT INTO users (name, email, phone) VALUES (?, ?, ?)',
+                                   (name, email, phone))
+                connection.commit()
+                connection.close()
+                flash('form successfully sent')
+                return redirect(url_for('about'))
+            except sqlite3.IntegrityError:
+                flash('User with this email already exist')
+                return redirect(url_for('about'))
     return render_template('about.html')
 
 @app.route('/admin')
 def admin():
-    return render_template('admin.html', user_messages=messages)
+    connection = db_connect()
+    users = connection.execute('SELECT * FROM users')
+    return render_template('admin.html', users=users)
